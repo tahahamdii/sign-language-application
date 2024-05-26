d6f097adb95d59786296cc5be25e1475240ef537
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-05-27 01:11:27 +0200

    camera implementation

--- lib/screens/chat/chat_page.dart
+++ lib/screens/chat/chat_page.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:stomp_dart_client/src/stomp_frame.dart';
+import 'package:http/http.dart' as http;
 import 'package:stomp_dart_client/stomp_dart_client.dart';
+import 'package:camera/camera.dart';
 
 class ScreenChat extends StatefulWidget {
   final String currentUserId;
   final String contactId;
-  const ScreenChat(
-      {Key? key, required this.currentUserId, required this.contactId})
-      : super(key: key);
+  const ScreenChat({
+    Key? key,
+    required this.currentUserId,
+    required this.contactId,
+  }) : super(key: key);
 
   @override
   _ScreenChatState createState() => _ScreenChatState();
@@ -19,6 +21,9 @@ class _ScreenChatState extends State<ScreenChat> {
   late StompClient _client;
   final TextEditingController _controller = TextEditingController();
   List<Map<String, dynamic>> messages = [];
+  String contactUsername = '';
+  late CameraController _cameraController;
+  late Future<void> _initializeCameraControllerFuture;
 
   @override
   void initState() {
@@ -31,6 +36,14 @@ class _ScreenChatState extends State<ScreenChat> {
       ),
     );
     _client.activate();
+
+    // Fetch chat history and contact username when the screen initializes
+    fetchChatHistory();
+    fetchContactUsername();
+
+    // Initialize camera controller
+    _initializeCameraController();
+    _initializeCameraControllerFuture = _initializeCameraController();
   }
 
   void _onConnectCallback(StompFrame connectFrame) {
@@ -45,7 +58,6 @@ class _ScreenChatState extends State<ScreenChat> {
                   receivedMessage['recipientId'] == widget.currentUserId)) {
             setState(() {
               messages.add(receivedMessage);
-              print(widget.currentUserId);
             });
           }
         }
@@ -53,11 +65,10 @@ class _ScreenChatState extends State<ScreenChat> {
     );
   }
 
-  void _sendMessage() {
-    final message = _controller.text;
-    if (message.isNotEmpty) {
+  void _sendMessage(String messageContent) {
+    if (messageContent.isNotEmpty) {
       final messageJson = {
-        'content': message,
+        'content': messageContent,
         'senderId': widget.currentUserId,
         'recipientId': widget.contactId,
         'timestamp': DateTime.now().toIso8601String(),
@@ -69,48 +80,166 @@ class _ScreenChatState extends State<ScreenChat> {
       setState(() {
         messages.add(messageJson);
       });
-      _controller.clear();
+    }
+  }
+
+  // Function to fetch chat history from the backend
+  void fetchChatHistory() {
+    final url =
+        'http://192.168.56.1:8080/messages/${widget.currentUserId}/${widget.contactId}';
+    http.get(Uri.parse(url)).then((response) {
+      if (response.statusCode == 200) {
+        List<dynamic> jsonResponse = json.decode(response.body);
+        List<Map<String, dynamic>> chatHistory =
+            List<Map<String, dynamic>>.from(jsonResponse);
+
+        setState(() {
+          messages.addAll(chatHistory);
+        });
+      } else {
+        print('Failed to fetch chat history: ${response.statusCode}');
+      }
+    }).catchError((error) {
+      print('Error fetching chat history: $error');
+    });
+  }
+
+  // Function to fetch contact's username
+  void fetchContactUsername() {
+    final url =
+        'http://192.168.56.1:8080/api/users/username/${widget.contactId}';
+    http.get(Uri.parse(url)).then((response) {
+      if (response.statusCode == 200) {
+        setState(() {
+          contactUsername = response.body;
+        });
+      } else {
+        print('Failed to fetch contact username: ${response.statusCode}');
+      }
+    }).catchError((error) {
+      print('Error fetching contact username: $error');
+    });
+  }
+
+  Future<void> _initializeCameraController() async {
+    // Obtain a list of available cameras on the device
+    final cameras = await availableCameras();
+
+    // Get the first camera from the list
+    final firstCamera = cameras.first;
+
+    // Initialize the camera controller
+    _cameraController = CameraController(
+      firstCamera,
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the camera controller
+    _initializeCameraControllerFuture = _cameraController.initialize();
+  }
+
+  Future<void> _openCamera() async {
+    try {
+      // Ensure that the camera is initialized before opening it
+      await _initializeCameraControllerFuture;
+
+      // Open the camera and await the result
+      final XFile? pickedFile = await _cameraController.takePicture();
+
+      // Do something with the taken picture (e.g., upload it or display it)
+      if (pickedFile != null) {
+        print('Image picked: ${pickedFile.path}');
+      } else {
+        print('No image picked');
+      }
+    } catch (e) {
+      // Handle errors that occur during camera opening
+      print('Error opening camera: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height - 250;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat with ${widget.contactId}'),
+        title: Text('Chat with $contactUsername'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Form(
-              child: TextFormField(
-                controller: _controller,
-                decoration: const InputDecoration(labelText: 'Send a message'),
-              ),
-            ),
-            const SizedBox(height: 24),
             Expanded(
               child: ListView.builder(
                 itemCount: messages.length,
                 itemBuilder: (context, index) {
                   Map<String, dynamic> item = messages[index];
-                  return ListTile(
-                    title: Text(item['content'] ?? 'No content'),
-                    subtitle: Text(item['timestamp'] ?? 'No timestamp'),
+                  bool isCurrentUser = item['senderId'] == widget.currentUserId;
+                  CrossAxisAlignment alignment = isCurrentUser
+                      ? CrossAxisAlignment.end
+                      : CrossAxisAlignment.start;
+                  Color backgroundColor =
+                      isCurrentUser ? Colors.blue : Colors.grey;
+
+                  return Align(
+                    alignment: isCurrentUser
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 4),
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: backgroundColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            item['content'] ?? 'No content',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            item['timestamp'] ?? 'No timestamp',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   );
                 },
               ),
             ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      labelText: 'Send a message',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.camera_alt),
+                        onPressed: _openCamera,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => _sendMessage(_controller.text),
+                ),
+              ],
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _sendMessage,
-        tooltip: 'Send message',
-        child: const Icon(Icons.send),
       ),
     );
   }
@@ -119,6 +248,7 @@ class _ScreenChatState extends State<ScreenChat> {
   void dispose() {
     _client.deactivate();
     _controller.dispose();
+    _cameraController.dispose(); // Dispose camera controller
     super.dispose();
   }
 }
